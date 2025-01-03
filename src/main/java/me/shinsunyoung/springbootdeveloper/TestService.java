0d89c3fdcfa3760ce1382f5ef45b1d126bc4a218
Choi BoKeun <cbk121779@gmail.com>
2025-01-03 20:18:09 +0900

    [feat] 3장. 오타 수정, 빠진 어노테이션 수정

--- src/main/java/me/shinsunyoung/springbootdeveloper/TestService.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/TestService.java
@@ -1,9 +1,10 @@
 package me.shinsunyoung.springbootdeveloper;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class TestService {
     @Autowired
     MemberRepository memberRepository;  // 빈 주입
